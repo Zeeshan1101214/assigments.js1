07e6ce7283631696caedc52d582894ff70eb5c76
zeeshan110 <zh1101214@g,ail.com>
2024-02-23 07:05:42 +0500

    work done

---
+++ app.js
@@ -0,0 +1,76 @@
+alert("Zeeshan-Harider")
+console.log('calculator')
+document.write ("<h1>"+"Calculator"+"</h1>")
+
+var num1 = prompt("Enter your 1st value")
+var num2 = prompt("Enter your 2st value")
+
+var add = num1 + num2;
+var minus = num1 - num2;
+var MULTIPLICATION = num1 * num2;
+var divide = num1 / num2;
+var percentage = num1 % num2;
+var root = num1 ^ num2;
+var log = num1 ^ num2;
+var tan = num1 ^ num2;
+var cos = num1 ^ num2;
+var sin = num1 ^ num2;  
+
+document.write(
+    "<table border = '2px'>"+
+    "<tr>"+
+    +"<th>"+"ADDITION "+"</th>"
+
+    +"<th>"+"MINUS"+"</th>"
+    +"<th>"+"MULTIPLICATION "+"</th>"
+    +"<th>"+"DIVISSION"+"</th>"
+    +"<th>"+"PERCENTAGE"+"</th>"
+    +"<th>"+"ROOT"+"</th>"
+    +"<th>"+"LOG"+"</th>"
+
+   + "<th>"+"TAN"+"</th>"
+   + "<th>"+"COS"+"</th>"
+   + "<th>"+"SIN"+"</th>"
+
+    
+    
+    
+    
+   + "</tr>"+
+
+
+    +"<tr>"+
+
+    +"<td>"+add+"</td>"
+   + "<td>"+minus+"</td>"
+    +"<td>"+MULTIPLICATION +"</td>"
+    +"<td>"+divide+"</td>"
+   + "<td>"+percentage+"</td>"
+   +"<td>"+root+"</td>"
+   +"<td>"+log+"</td>"
+
+  +"<td>"+tan+"</td>"
+  + "<td>"+cos+"</td>"
+  + "<td>"+sin+"</td>"
+
+    +"</tr>"
+ 
+    +"</tr>"
+
+
+
+
+
+
+
+
+
+
+
+
+
++"</table>"
+
+)
+
+
